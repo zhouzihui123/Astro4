eb08d889dbfa3d9c1b990a8657d0c45b6ab95976
zhouzihui123 <2473005809@qq.com>
2024-06-09 14:52:36 +0800

    修改

--- superior-shepherd/astro.config.mjs
+++ superior-shepherd/astro.config.mjs
@@ -1,7 +1,4 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({
-  output: "server",
-  adapter: vercel()
-});
+// astro.config.mjs  
+import { defineConfig } from 'astro/config';  
+  
+export default defineConfig({});
